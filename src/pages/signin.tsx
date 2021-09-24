b91108934347aded1a083536e7c61c74d4030996
gnatal <guilhermenatal47@gmail.com>
2021-09-24 15:35:19 -0300

    Setup with eslint and absolute paths

---
+++ src/pages/signin.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import type { NextPage } from 'next'
+import styles from 'styles/Home.module.css'
+
+const SignIn: NextPage = () => (
+    <div className={styles.container}>
+        first page
+    </div>
+)
+
+export default SignIn;
